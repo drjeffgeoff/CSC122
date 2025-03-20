2ef8b5cfdf227ff24cafd0d813a976be01e8ea41
drjeffgeoff <jeff.geoff.phd@gmail.com>
2025-03-20 04:44:57 +0300

    Added

---
+++ programc.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Preprocessor Directive
+
+int main()
+{                              // Main function
+    printf("Hello, World!\n"); // Output function
+    return 0;                  // Exit status
+}
